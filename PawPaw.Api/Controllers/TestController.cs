6aaa87735fa560483406b8dd6b6af400b2212891
Mads Nyborg <nyborg.mads@gmail.com>
2016-04-01 15:54:20 +0200

    added api project

---
+++ PawPaw.Api/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PawPaw.Api.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("test")]
+        public string GetStuff()
+        {
+            return "HEIEHEI";
+        }
+    }
+}
